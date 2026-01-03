f8d384b3302437e5574f0cb429945d8d9e671a86
nguyenvinh1234 <quangcaomarketing2016@gmail.com>
2026-01-03 22:42:54 +0700

    Update __manifest__.py

--- llm_knowledge_llama/__manifest__.py
+++ llm_knowledge_llama/__manifest__.py
@@ -7,7 +7,7 @@
         document segments for more accurate AI retrieval. Turn structured documents into AI-friendly chunks.
     """,
     "category": "Technical",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "depends": ["llm_knowledge"],
     "external_dependencies": {
         "python": ["llama_index", "nltk"],
